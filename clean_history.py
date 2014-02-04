c40e703ea6003930cf2fb483e7c97c4a082c2fe9
Thomas Desvenain <thomas.desvenain@gmail.com>
2014-02-04 13:26:32 +0100

    more robust script

--- clean_history.py
+++ clean_history.py
@@ -5,14 +5,15 @@ import optparse
 import transaction
 
 from ZODB.POSException import ConflictError
-from Products.CMFEditions.utilities import dereference
-
 from AccessControl.SecurityManagement import newSecurityManager
 from AccessControl.SecurityManager import setSecurityPolicy
+from zope.component.hooks import setSite
 from Testing.makerequest import makerequest
+
 from Products.CMFCore.tests.base.security import PermissiveSecurityPolicy, OmnipotentUser
-#from Products.CMFEditions.utilities import isObjectVersioned
 from Products.Archetypes.utils import shasattr
+from Products.CMFEditions.utilities import dereference
+
 
 version = '0.2.1'
 usage = "usage: /your/instance run clean_history.py [options] [sites]"
@@ -30,6 +31,9 @@ p.add_option('--verbose', '-v', action="store_true", default=False,
              help="Show verbose output, for every cleaned content's history.")
 
 args = sys.argv[1:]
+args = [arg for arg in args
+        if arg != '-c' and not arg.endswith('clean_history.py')]
+
 options, psite = p.parse_args(args)
 pp_type = options.portal_types
 
@@ -53,11 +57,13 @@ def spoofRequest(app):
 # Enable Faux HTTP request object
 app = spoofRequest(app)
 
-sites = [(id, site) for (id, site) in app.items() if hasattr(site, 'meta_type') and site.meta_type=='Plone Site']
+sites = [(id, site) for (id, site) in app.items()
+         if hasattr(site, 'meta_type') and site.meta_type=='Plone Site']
 
 print 'Starting analysis for %s. Types to cleanup: %s' % (not psite and 'all sites' or ', '.join(psite),
                                                           not pp_type and 'all' or ', '.join(pp_type))
 for id, site in sites:
+    setSite(site)
     if not psite or id in psite:
         print "Analyzing %s" % id
         policy = site.portal_purgepolicy
@@ -74,9 +80,10 @@ for id, site in sites:
 
         catalog = site.portal_catalog
         if pp_type:
-            results = catalog(portal_type=pp_type)
+            results = catalog.unrestrictedSearchResults(portal_type=pp_type)
         else:
-            results = catalog()
+            results = catalog.unrestrictedSearchResults()
+
         for x in results:
             if options.verbose:
                 print "... cleaning history for %s (%s)" % (x.getPath(), x.portal_type)
@@ -89,7 +96,7 @@ for id, site in sites:
                     if shasattr(obj, 'version_id') and options.keep_history<=1:
                         del obj.version_id
                     if options.verbose:
-                        print "... cleaned!" 
+                        print "... cleaned!"
             except ConflictError:
                 raise
             except Exception, inst:
